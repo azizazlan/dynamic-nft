b27b27cd77cfbf77e905c384afaf54fb44363aa3
Azlan Aziz <azizazlan@gmail.com>
2022-03-10 14:57:13 +0800

    test createCollectible

--- test/index.ts
+++ test/index.ts
@@ -12,6 +12,12 @@ describe('WuWeiNFT', () => {
   const MOCK_SUBSCRIPTION_ID = '0';
   const MOCK_LINK = constants.AddressZero;
 
+  const URIs = [
+    'https://ipfs.io/ipfs/QmZ8epAYRBVgmC89AkhMcYTvSXqaXoVyY1wDejcts8YfrF?filename=metadata1.json',
+    'https://ipfs.io/ipfs/QmYouRy6h83ifpmTx4MpN6rhg3ByCxhDBCAYsBFWPngjhX?filename=metadata2.json',
+    'https://ipfs.io/ipfs/QmVsTvG8m5pZFCG9oaTRCxfFFzEHzi1YfptTgqX1h6xjKu?filename=metadata3.json',
+  ];
+
   async function deployContract(
     vrfCoordinatorContract:
       | 'MockVRFCoordinator'
@@ -46,15 +52,42 @@ describe('WuWeiNFT', () => {
     });
   });
 
-  describe('AccessControl to createCollectible function', () => {
+  // function createCollectible() public onlyRole(MINTER_ROLE) {
+  //   uint256 tokenId = _tokenIdCounter.current();
+  //   _tokenIdCounter.increment();
+
+  //   string memory initialUri = IpfsUri[2];
+  //   EigenValue initialEigenVal = EigenValue(2);
+  //   _safeMint(msg.sender, tokenId);
+  //   _setTokenURI(tokenId, initialUri);
+  //   tokenIdToEigenValue[tokenId] = initialEigenVal;
+  // }
+  describe('createCollectible', () => {
     it('Only owner has default admin and minter roles', async () => {
       await contract.connect(owner).createCollectible();
       let totalSupply = await contract.totalSupply();
       expect(totalSupply).equal(ethers.BigNumber.from(1));
 
+      const tokenId_0 = await contract.tokenByIndex(0);
+      expect(tokenId_0).equal(ethers.BigNumber.from(0));
+
       await contract.connect(owner).createCollectible();
       totalSupply = await contract.totalSupply();
       expect(totalSupply).equal(ethers.BigNumber.from(2));
+
+      const tokenId_1 = await contract.tokenByIndex(1);
+      expect(tokenId_1).equal(ethers.BigNumber.from(1));
+    });
+    it('tokenURI should retrieves the last index', async () => {
+      let tokenId = 0;
+      await contract.connect(owner).createCollectible();
+      let uri = await contract.tokenURI(tokenId);
+      expect(uri).equal(URIs[2]);
+    });
+    it('owner', async () => {
+      let tokenId = 0;
+      await contract.connect(owner).createCollectible();
+      expect(await contract.ownerOf(tokenId)).equal(owner.address);
     });
     it('Non owner should not be able to call', async () => {
       await expect(contract.connect(nonOwner).createCollectible()).to.be
